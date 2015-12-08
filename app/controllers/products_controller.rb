c5f938fa09e9eb39a2777cda7ea3b553899a3d08
noglows <jessica.noglows@outlook.com>
2015-12-08 14:09:13 -0800

    Set-up for Account Details page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    user_id = session[:user_id]
-      if user_id.nil?
+    @user_id = session[:user_id]
+      if @user_id.nil?
         @user_name = "Guest"
       else
-        @user_name = User.find(user_id).first_name
+        @user_name = User.find(@user_id).first_name
       end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(session[:user_id])
+  end
+
   private
 
   def user_params
