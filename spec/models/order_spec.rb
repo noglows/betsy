45c3a138cb49f969a6b476744d29567eaa7f0216
noglows <jessica.noglows@outlook.com>
2015-12-08 12:20:49 -0800

    Wrote RSpec tests for all models

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:order_valid) { Order.new(status: "test", user_id: 2)}
+
+  describe ".validates" do
+    it "must have a user id" do
+      order = Order.new(status:"test")
+      expect(order).to_not be_valid
+      expect(order.errors.keys).to include :user_id
+      expect(order_valid).to be_valid
+    end
+
+    it "must have a status" do
+      order = Order.new(user_id: 2)
+      expect(order).to_not be_valid
+      expect(order.errors.keys).to include :status
+      #expect(order_valid).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:password) { BCrypt::Password.create("password") }
+
+  let(:user_valid) { User.new(email: "test@test.com", password: password) }
+
+  describe ".validates" do
+    it "must have a email" do
+      user = User.new(password: password)
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include :email
+      expect(user_valid).to be_valid
+    end
+
+    it "must have a unique email" do
+      user_first = User.new(email:"test@test.com", password: password)
+      user_first.save
+      expect(user_valid).to_not be_valid
+      expect(user_valid.errors.keys).to include :email
+    end
+
+    it "must have a valid email format" do
+      user = User.new(password: password, email: "test")
+      expect(user).to_not be_valid
+      expect(user.errors.keys).to include :email
+    end
+  end
 end
