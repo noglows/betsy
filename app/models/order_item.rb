20cf40939c36f947f4f19a0c635179fce368e8c8
noglows <jessica.noglows@outlook.com>
2015-12-08 11:44:24 -0800

    Set up the model validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,10 @@
 class OrderItem < ActiveRecord::Base
+  belongs_to :order
+  belongs_to :product
+
+  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { only_integer: true }
+  validates :product_id, presence: true
+  validates :order_id, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :order_items
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :user_id, presence: true
 end
