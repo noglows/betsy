1c3a0c1779341423d42197c4cb4620bd2dfa4515
Daphne <dg1181@nyu.edu>
2015-12-08 12:19:27 -0800

    Uncommented bcrypt, added seeds for user and category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_categories = [
+  { name: "stuffed animals" },
+  { name: "stickers" },
+  { name: "clothing" },
+  { name: "slippers" },
+  { name: "socks" },
+]
+
+seed_categories.each do |seed|
+  Category.create(seed)
+end
+
+seed_users = [
+  { first_name: "Daphne", last_name: "Gold", email: "1@1.com" },
+  { first_name: "Amy", last_name: "Hunter", email: "2@2.com" },
+  { first_name: "Jessica", last_name: "Noglows", email: "3@3.com" },
+  { first_name: "Lauren", last_name: "Granger", email: "4@4.com" },
+  { first_name: "Mystery Person", last_name: "Mystery", email: "5@5.com" }
+]
+
+seed_users.each do |seed|
+  user = User.create(seed)
+  user.password = "1"
+  user.save
+end
