bf1d14121834de32c6f596a0a0306b65b9758d11
noglows <jessica.noglows@outlook.com>
2015-12-09 10:30:45 -0800

    Fixed retired functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
   root 'products#index'
 
   resources :products do
-    post "/review" , to: "products#review"
+    post "/review", to: "products#review"
   end
 
   resources :orders, only: [:show, :update]
 
   resources :users do
     resources :orders
-    resources :products
+    resources :products do
+      post "/retire", to: "products#retire"
+    end
   end
 
   resources :sessions, :only => [:new, :create]
