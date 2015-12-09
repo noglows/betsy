62f7bf99e930ae2c95e6920472baf4713d33b6b7
Amy Hunter <amyrhunter@gmail.com>
2015-12-09 14:31:24 -0800

    Fixed some tests in users and products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
       else
         @user_name = User.find(@user_id).first_name
       end
-    @categoies = Category.all
+    @categories = Category.all
     @merchants = User.all
     case params[:order]
     when "prod"
