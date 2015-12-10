9f777885ca8382758d22333f44df6176741f34c5
noglows <jessica.noglows@outlook.com>
2015-12-10 11:39:25 -0800

    Orders are in order of date

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,21 @@ class OrdersController < ApplicationController
   def index
     user_id = params[:user_id]
     @user = User.find(user_id)
+    @orders = []
+    @user.products.each do |product|
+      product.order_items.each do |oi|
+        @orders.push(oi.order)
+      end
+    end
+    @orders.sort_by! { |obj| obj.updated_at }
+    return @orders
   end
 
   def show
     user_id = params[:user_id]
     order_id = params[:id]
     @order = Order.find(order_id)
+
+
   end
 end
