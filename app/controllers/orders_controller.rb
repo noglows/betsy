2711a87c8a77ec1a72c2f283fde61f3688c0f28d
noglows <jessica.noglows@outlook.com>
2015-12-10 12:01:56 -0800

    Orders only displayed once on the orders index page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,17 +7,23 @@ class OrdersController < ApplicationController
     @orders = []
     @user.products.each do |product|
       product.order_items.each do |oi|
-        @orders.push(oi.order)
+        if @orders.include? oi.order
+          next
+        else
+          @orders.push(oi.order)
+        end
       end
     end
     @orders.sort_by! { |obj| obj.updated_at }
     return @orders
+
   end
 
   def show
     user_id = params[:user_id]
     order_id = params[:id]
     @order = Order.find(order_id)
+    @user = User.find(user_id)
 
 
   end
