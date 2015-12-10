3cea20b2def2ee41ef0d60fc104216b88325a4e0
noglows <jessica.noglows@outlook.com>
2015-12-10 14:05:26 -0800

    Updated the model validation tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,15 +5,9 @@ RSpec.describe Order, type: :model do
   let(:order_valid) { Order.new(status: "test", user_id: 2)}
 
   describe ".validates" do
-    it "must have a user id" do
-      order = Order.new(status:"test")
-      expect(order).to_not be_valid
-      expect(order.errors.keys).to include :user_id
-      expect(order_valid).to be_valid
-    end
 
     it "must have a status" do
-      order = Order.new(user_id: 2)
+      order = Order.new()
       expect(order).to_not be_valid
       expect(order.errors.keys).to include :status
       #expect(order_valid).to be_valid
