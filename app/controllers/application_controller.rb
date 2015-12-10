4cca41fca4e061c1e01241a4d5b1e8b0252ce801
Daphne <dg1181@nyu.edu>
2015-12-09 19:55:55 -0800

    test with guest user beginning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) || guest_user
+  end
+
+  def guest_user
+    @guest ||= Guest.new(session)
   end
 end
