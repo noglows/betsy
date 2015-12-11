a63ee91b6eea760ef347216fdd6baaf012d8391d
noglows <jessica.noglows@outlook.com>
2015-12-11 14:37:20 -0800

    formatted the CC expiration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,36 @@ module ApplicationHelper
   def cents_to_dollars(money)
     number_to_currency(money/100)
   end
+
+  def cc_expiration(date)
+    month = date[0..1]
+    year = date[2..3]
+    year = "20" + year
+    if month[0] = "0"
+      month = month[1]
+    end
+    month = month.to_i
+    monthnames =
+      {
+        1 => "January",
+        2 => "February",
+        3 => "March",
+        4 => "April",
+        5 => "May",
+        6 => "June",
+        7 => "July",
+        8 => "August",
+        9 => "September",
+        10 => "October",
+        11 => "November",
+        12 => "December"
+      }
+
+    month = monthnames[month]
+    "#{month} #{year}"
+  end
+
+
+
+
 end
