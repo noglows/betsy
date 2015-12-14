74d4b0d6dd612b61d87250fcc56982acd5089f6d
Lauren Granger <lr.granger21@gmail.com>
2015-12-14 14:39:36 -0800

    passing spec for creating new order when first order item created

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -18,14 +18,26 @@ RSpec.describe OrderItemsController, type: :controller do
         product_id: 6,
       }
     end
+
     before :each do
-      cookies[:order] = nil
+      cookies.signed[:order] = nil
     end
 
     it "creates a new order instance when one does not already exist" do
       all_orders = Order.all.to_a
       post :create, order_item_params
       expect(all_orders).not_to include(Order.all.last)
+    end
+
+    before :each do
+      ord = Order.create(status:"pending")
+      cookies.signed[:order] = ord.id
+    end
+
+    it "creates a new order instance when one does not already exist" do
+      all_orders = Order.all.length
+      post :create, order_item_params
+      expect(all_orders).to eq(Order.all.length)
     end
 
     it "redirects to the cart path when successful" do
