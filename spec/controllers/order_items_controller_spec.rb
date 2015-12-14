39858ea81838e6b2e5db44717bf6f0b6e802b3d7
Amy Hunter <amyrhunter@gmail.com>
2015-12-14 11:41:24 -0800

    More trying to write tests in the order items controller

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe OrderItemsController, type: :controller do
-  let(:order_item)do
-    OrderItem.create(quantity: 12, order_id: 1, product_id: 6)
-  end
-
   describe "POST 'create'" do
+    let(:order_item_parameters) do
+      {
+        order_item: {
+          quantity: 12,
+        },
+        product_id: {
+          order_id: nil,
+          product_id: 6
+        },
+      }
+    end
+    before :each do
+      cookies[:order] = nil
+    end
+
+    it "creates a new order instance when one does not already exist" do
+      all_orders = Order.all
+      post :create, product_id: order_item_params
+      expect(all_orders.include?(@order)).to be_false
+    end
+
+    it "redirects to the cart path when successful" do
+      post :create, order_item_params
+      expect(subject).to redirect_to cart_path
+    end
 
   end
 
