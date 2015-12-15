df6cb72a041acba0053ac89faa282530ca3bacc1
noglows <jessica.noglows@outlook.com>
2015-12-15 10:29:25 -0800

    Small controller bug fix - categories were broken

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class ProductsController < ApplicationController
   before_action :current_user
   before_action :require_login, only: [:new, :create, :edit, :update, :retire]
@@ -15,7 +13,7 @@ class ProductsController < ApplicationController
     when "cat"
       prod = Product.all
       @products = prod.select {|product|
-        rows = Product.categories.where("category_id = #{params[:order]}")
+        rows = product.categories.where("category_id = #{params[:order]}")
         !rows.to_a.empty?
       }
     else
@@ -33,7 +31,6 @@ class ProductsController < ApplicationController
 
   def review
     Review.create(review_params)
-
     redirect_to product_path(params[:product_id])
   end
 
@@ -78,12 +75,6 @@ class ProductsController < ApplicationController
       render :edit
     end
   end
-
-  # def destroy
-  #   product_id = params[:id]
-  #   Product.destroy(product_id)
-  #   redirect_to user_path(params[:user_id])
-  # end
 
   def retire
     product_id = params[:product_id]
