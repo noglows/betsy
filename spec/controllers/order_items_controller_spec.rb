116311098501ea3f7e7b05505370a68153e00100
Lauren Granger <lr.granger21@gmail.com>
2015-12-15 12:11:17 -0800

    spec added and IS PASSING for update does not destroy when quantity is not 0

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -82,6 +82,22 @@ RSpec.describe OrderItemsController, type: :controller do
       expect(OrderItem.all.length).to_not eq(how_many_order_items_before)
     end
 
+    let(:order_item_params_two) do
+      {
+        order_item: {
+          quantity: 1,
+        },
+        product_id: @product.id,
+        id: @order_item.id,
+      }
+    end
+
+    it "does not destory the order item if the quantity is not 0" do
+      how_many_order_items_before = OrderItem.all.to_a.length
+      patch :update, order_item_params_two
+      expect(OrderItem.all.length).to eq(how_many_order_items_before)
+    end
+
   end
 
   describe "DELETE 'destroy'" do
