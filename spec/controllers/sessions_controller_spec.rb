5a22034a44c61777db015653703694e8815a8aa0
Amy Hunter <amyrhunter@gmail.com>
2015-12-15 14:59:43 -0800

    More fiddling. Tests are so broken.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "POST 'create'" do
+    let(:session) do
+      session[:user_id] = nil
+    end
 
+    it "sets the user id of the session to the appropriate user id" do
+
+    end
   end
 
   describe "DELETE 'destroy'" do
+    it "sets the user id of the session to nil" do
+
+    end
 
   end
 end
