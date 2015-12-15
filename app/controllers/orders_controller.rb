067c896acdf2b02796aa0aa00880be38dd6b3b6d
Lauren Granger <lr.granger21@gmail.com>
2015-12-15 13:57:10 -0800

    spec written and NOT PASSING for index renders index view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,32 +4,32 @@ class OrdersController < ApplicationController
   # before_action :check_user_id, only: [:index, :show, :ship]
 
 
-  def index
-    user_id = params[:user_id]
-    @user = User.find(user_id)
-    @orders = []
-    @user.products.each do |product|
-      product.order_items.each do |oi|
-        if ['pending','paid','cancelled','complete'].include? params[:sort]
-          if oi.order.status == params[:sort]
-            if @orders.include? oi.order
-              next
-            else
-              @orders.push(oi.order)
-            end
-          end
-        else
-          if @orders.include? oi.order
-            next
-          else
-            @orders.push(oi.order)
-          end
-        end
-      end
-    end
-    @orders.sort_by! { |obj| obj.updated_at }
-    return @orders
-  end
+  # def index
+  #   user_id = params[:user_id]
+  #   @user = User.find(user_id)
+  #   @orders = []
+  #   @user.products.each do |product|
+  #     product.order_items.each do |oi|
+  #       if ['pending','paid','cancelled','complete'].include? params[:sort]
+  #         if oi.order.status == params[:sort]
+  #           if @orders.include? oi.order
+  #             next
+  #           else
+  #             @orders.push(oi.order)
+  #           end
+  #         end
+  #       else
+  #         if @orders.include? oi.order
+  #           next
+  #         else
+  #           @orders.push(oi.order)
+  #         end
+  #       end
+  #     end
+  #   end
+  #   @orders.sort_by! { |obj| obj.updated_at }
+  #   return @orders
+  # end
 
   def show
     user_id = params[:user_id]
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -14,30 +14,32 @@ RSpec.describe OrdersController, type: :controller do
   end
 
   describe "GET 'index'" do
-
-  end
-
-  describe "GET 'new'" do
-    it "renders the new view" do
-      get :new
-      expect(subject).to render_template :new
+    it "renders the index view" do
+      expect(subject).to render_template :index
     end
   end
 
-  describe "GET 'show'" do
-    it "renders the show page" do
-
-    end
-
-  end
-
-  describe "PATCH 'update'" do
-
-  end
-
-  describe "PATCH 'ship'" do
-
-
-  end
+  # describe "GET 'new'" do
+  #   it "renders the new view" do
+  #     get :new
+  #     expect(subject).to render_template :new
+  #   end
+  # end
+  #
+  # describe "GET 'show'" do
+  #   it "renders the show page" do
+  #
+  #   end
+  #
+  # end
+  #
+  # describe "PATCH 'update'" do
+  #
+  # end
+  #
+  # describe "PATCH 'ship'" do
+  #
+  #
+  # end
 
 end
