ec56507ddfd999281984db7ddf0f2a38f460c08c
Lauren Granger <lr.granger21@gmail.com>
2015-12-15 12:04:19 -0800

    spec set, but NOT PASSING for update destroys when quantity is 0

--- spec/controllers/order_items_controller_spec.rb
+++ spec/controllers/order_items_controller_spec.rb
@@ -74,6 +74,22 @@ RSpec.describe OrderItemsController, type: :controller do
       expect(subject).to redirect_to cart_path
     end
 
+    let(:order_item_params) do
+      {
+        order_item: {
+          quantity: 0,
+        },
+        product_id: @product.id,
+        id: @order_item.id,
+      }
+    end
+
+    it "destroys the order item if the quantity is 0" do
+      how_many_order_items_before = OrderItem.all.to_a.length
+      patch :update, order_item_params
+      expect(OrderItem.all.length).to_not eq(how_many_order_items_before)
+    end
+
   end
 
   describe "DELETE 'destroy'" do
