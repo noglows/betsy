d98d87e3b2b96b042189c327883c556d5ccc2e79
Amy Hunter <amyrhunter@gmail.com>
2015-12-16 13:32:51 -0800

    Add working tests for orders controller for the index and show actions

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -10,21 +10,54 @@ RSpec.describe OrdersController, type: :controller do
   end
 
   let(:order) do
-    Order.create(status:"pending", user_id: 4)
+    Order.create(status: "pending",
+                 email: "caprina.keller@test.com",
+                 mailing_address: "3158 Union Street,
+                 Reisterstown, MD",
+                 name_on_card: "Caprina Keller",
+                 last_four: "1911",
+                 card_exp: Date.today,
+                 zip: "22136"
+    )
   end
 
   describe "GET 'index'" do
+    it "renders the index view" do
+      get :index, user_id: user.id
+      expect(subject).to render_template :index
+    end
 
   end
 
   describe "GET 'show'" do
     it "renders the show page" do
-
+      get :show, id: order.id, user_id: user.id
+      expect(subject).to render_template :show
     end
   end
 
   describe "GET 'checkout'" do
-    
+    before :suite do
+      cookies.signed[:order] = order.id
+      # cookies.signed[:stocked] = order.length
+      session[:order] = nil
+    end
+
+    it "renders checkout view if cart is not empty" do
+      order_item = OrderItem.create(quantity: 4,
+                         product_id: 5,
+                         shipped: false,
+                         order_id: order.id)
+      product = Product.create(name: "Fuzzy Wah-Wah pedal",
+                         description: "Making some glorious vintage guitar sounds",
+                         price: 4000,
+                         inventory_total: 50,
+                         retired: false,
+                         image_url: "http://i.ebayimg.com/images/i/251806872987-0-1/s-l1000.jpg",
+                         user_id: user.id)
+      get :checkout
+      expect(subject).to render_template :checkout
+    end
   end
 
   describe "PATCH 'update'" do
