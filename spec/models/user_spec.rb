5459c03d7dcd462dcc0ea3d9174a1e9ed8d1356b
noglows <jessica.noglows@outlook.com>
2015-12-18 09:25:03 -0800

    Fixed failing user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe User, type: :model do
 
   let(:password) { BCrypt::Password.create("password") }
 
-  let(:user_valid) { User.new(email: "test@test.com", password: password) }
+  let(:user_valid) { User.new(email: "test@test.com", password: password, first_name: "Jessica", last_name: "Test") }
 
   describe ".validates" do
 
     it "must have a email" do
-      user = User.new(password: password)
+      user = User.new(password: password, first_name: "Jessica", last_name: "Test")
       expect(user).to_not be_valid
       expect(user.errors.keys).to include :email
       expect(user_valid).to be_valid
     end
 
     it "must have a unique email" do
-      user_first = User.new(email:"test@test.com", password: password)
+      user_first = User.new(email:"test@test.com", password: password, first_name: "Jessica", last_name: "Test")
       user_first.save
       expect(user_valid).to_not be_valid
       expect(user_valid.errors.keys).to include :email
     end
 
     it "must have a valid email format" do
-      user = User.new(password: password, email: "test")
+      user = User.new(password: password, email: "test", first_name: "Jessica", last_name: "Test")
       expect(user).to_not be_valid
       expect(user.errors.keys).to include :email
     end
