b83ccbd12f13f602c82671df3076a6e2a5e511d2
Lauren Granger <lr.granger21@gmail.com>
2016-01-18 19:18:15 -0800

    move reverse logic from controller to model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ActiveRecord::Base
   validates :rating, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5}
   validates :product_id, presence: true
 
+  def self.reverse_by_product(params)
+    self.where("product_id = #{params[:id]}").reverse
+  end
+
 end
